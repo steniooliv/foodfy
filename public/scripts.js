b53dffddcfa579d8e3f80ec0a10679b36c6e37fa
Stenio Oliveira <steniooliv@gmail.com>
2022-02-15 20:12:37 -0300

    remove modal and create recipe description page

--- public/scripts.js
+++ public/scripts.js
@@ -1,27 +1,21 @@
-const modalOverlay = document.querySelector(".modal-overlay");
-const modal = document.querySelector(".modal");
+const toggleVisible = document.querySelectorAll(".isVisible");
+const paragraphs = document.querySelectorAll(".info");
 const cards = document.querySelectorAll(".card");
 
-for (let card of cards) {
-  card.addEventListener("click", function(){
-    const card_id = card.getAttribute("id");
-    const title = card.getElementsByClassName("title")[0].innerHTML;
-    const chef = card.getElementsByClassName("chef")[0].innerHTML;
-    modalOverlay.classList.add("active");
-
-    modal.querySelector("img").src = `assets/${card_id}.png`
-    modal.querySelector("img").alt = title
-    modal.getElementsByClassName("title")[0].innerHTML = title;
-    modal.getElementsByClassName("chef")[0].innerHTML = chef;
-  })
-  
+for (let h = 0; h < toggleVisible.length; h++) {
+  toggleVisible[h].addEventListener("click", () => {
+    if (paragraphs[h].classList.contains("hidden")) {
+      paragraphs[h].classList.remove("hidden");
+      toggleVisible[h].innerHTML = "Esconder";
+    } else {
+      paragraphs[h].classList.add("hidden");
+      toggleVisible[h].innerHTML = "Mostrar";
+    }
+  });
 }
 
-document.querySelector(".close").addEventListener("click", function(){
-  modalOverlay.classList.remove("active");
-
-  modal.querySelector("img").src = ""
-  modal.querySelector("img").alt = ""
-  modal.getElementsByClassName("title")[0].innerHTML = "";
-  modal.getElementsByClassName("chef")[0].innerHTML = "";
-});
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click", function(){
+    window.location.href = `/recipes/${i}`
+  })
+}
