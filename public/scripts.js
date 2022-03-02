3d22182503fbe15963c3a1cc1893074224a93b81
Stenio Oliveira <steniooliv@gmail.com>
2022-03-01 22:40:09 -0300

    add scripts to admin pages

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 const toggleVisible = document.querySelectorAll(".isVisible");
 const paragraphs = document.querySelectorAll(".info");
 const cards = document.querySelectorAll(".card");
@@ -14,8 +16,37 @@ for (let h = 0; h < toggleVisible.length; h++) {
   });
 }
 
-for (let i = 0; i < cards.length; i++) {
-  cards[i].addEventListener("click", function(){
-    window.location.href = `/recipes/${i}`
-  })
+function addIngredient() {
+  const ingredients = document.querySelector("#ingredients");
+  const fieldContainer = document.querySelectorAll(".ingredient");
+
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+  if (newField.children[0].value == "") return false;
+
+  newField.children[0].value = "";
+  ingredients.appendChild(newField);
+}
+
+document.querySelector(".add-ingredient").addEventListener("click", addIngredient);
+
+function addPreparation() {
+  const ingredients = document.querySelector("#preparations");
+  const fieldContainer = document.querySelectorAll(".preparation");
+
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+  if (newField.children[0].value == "") return false;
+
+  newField.children[0].value = "";
+  ingredients.appendChild(newField);
+}
+
+document.querySelector(".add-preparation").addEventListener("click", addPreparation);
+
+
+function deleteConfirm(e) {
+  if (!confirm("Deseja mesmo deletar?")) {
+    e.preventDefault();
+  };
 }
